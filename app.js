3f6ca4180ad493292c27a90193442c8fcefcfb54
Deekshith M D <deekshithmogra@gmail.com>
2021-12-01 11:31:03 +0530

    taking input

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-console.log("Script working");
-var name=prompt("Enter your name")
-alert(name)
+var btn=document.querySelector("#btn-translate")
+var text=document.querySelector("#text");
+
+btn.addEventListener("click",translate)
+function translate(){
+    alert("Text: "+text.value)
+}
