4265e6ec180817a0d83d6f4a0a878dca8ab89bf8
Deekshith M D <deekshithmogra@gmail.com>
2021-12-01 16:12:32 +0530

    feature: complete project

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var btn=document.querySelector("#btn-translate")
 var text=document.querySelector("textarea");
 var output=document.querySelector("#output")
-
 btn.addEventListener("click",translate)
+//var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var url="https://api.funtranslations.com/translate/minion.json"
+
+function errorHandler(){
+    alert("Some error occured...Please try after some time")
+}
+
+function construct(t){
+    return url+"?"+"text="+t;
+}
 function translate(){
-    output.innerText="Translated: "+text.value;
+    var inputText=text.value
+    fetch(construct(inputText))
+    .then(response=>response.json())
+    .then(json=>console.log(output.innerText=json.contents.translated))
+    .catch(errorHandler)
 }
