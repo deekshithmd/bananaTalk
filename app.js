0b0657d86874c7ccce58cd10339ac007d06fa0e8
Deekshith M D <deekshithmogra@gmail.com>
2021-12-01 16:42:28 +0530

    refactor: Updated code

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
+//Variables
 var btn=document.querySelector("#btn-translate")
 var text=document.querySelector("textarea");
 var output=document.querySelector("#output")
-btn.addEventListener("click",translate)
+
 //var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 var url="https://api.funtranslations.com/translate/minion.json"
 
+//Error handling function
 function errorHandler(){
     alert("Some error occured...Please try after some time")
 }
 
-function construct(t){
-    return url+"?"+"text="+t;
+//Constructing URL using input text
+function constructUrl(text){
+    return url+"?"+"text="+text;
 }
-function translate(){
+
+//language translation function
+function translateTo(){
     var inputText=text.value
-    fetch(construct(inputText))
+    fetch(constructUrl(inputText))
     .then(response=>response.json())
     .then(json=>console.log(output.innerText=json.contents.translated))
     .catch(errorHandler)
 }
+
+//Call
+btn.addEventListener("click",translateTo)
