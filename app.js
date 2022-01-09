35c97f142db260ee514f998b8c5b68dcc90da6f2
Deekshith M D <deekshithmogra@gmail.com>
2022-01-09 17:38:13 +0530

    refactor : es6

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //Variables
-var btn=document.querySelector("#btn-translate")
-var text=document.querySelector("textarea");
-var output=document.querySelector("#output")
+const btn=document.querySelector("#btn-translate")
+const text=document.querySelector("textarea");
+const output=document.querySelector("#output")
 
 //var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-var url="https://api.funtranslations.com/translate/minion.json"
+const url="https://api.funtranslations.com/translate/minion.json"
 
 //Error handling function
 const errorHandler=()=> alert("Some error occured...Please try after some time");
@@ -14,7 +14,7 @@ const constructUrl= text => url+"?"+"text="+text;
 
 //language translation function
 const translateTo=()=>{
-    var inputText=text.value
+    let inputText=text.value
     fetch(constructUrl(inputText))
     .then(response=>response.json())
     .then(json=>output.innerText=json.contents.translated)
